917eef357c81abc233460aa5a78cba2c5b73d522
Kevin Robb <kevin@js-x.com>
2020-12-09 16:47:38 -0600

    Modify write_file to make both readable and recoverable

--- decision-tree/dt_test_storage.py
+++ decision-tree/dt_test_storage.py
@@ -5,7 +5,7 @@ from decision_tree import DecisionTree
 import dt_util
 import sys
 
-def write_file(suffix:str,recoverable:bool=True):
+def write_file(suffix:str,recoverable:bool=True, readable:bool=False):
     # set tree name and get data
     df_train, df_val, df_test = dt_util.init_dt_data(suffix)
     # make the tree
@@ -17,7 +17,7 @@ def write_file(suffix:str,recoverable:bool=True):
     if recoverable:
         # write tree to file (recoverable)
         dtns.tree_to_file(root_node=root_node)
-    else:
+    if readable:
         # write tree to file (readable)
         test_pred = tree.predict_list(examples=df_test,root_node=root_node)
         acc = dt_util.accuracy(dt_util.get_labels(df_test),test_pred)
@@ -39,7 +39,7 @@ print("If write: Second argument: full_cat, seg_cat, full, or seg.")
 print("If read: Second argument: filename.")
 mode = str(sys.argv[1])
 if mode == "write":
-    write_file(suffix=str(sys.argv[2]))
+    write_file(suffix=str(sys.argv[2]), recoverable=True, readable=True)
 elif mode == "read":
     root_node = read_file(fname=str(sys.argv[2]))
 
